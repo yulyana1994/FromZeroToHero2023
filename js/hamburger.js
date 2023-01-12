dceedbfe83f1c4298ba1cdf1b505d9c8460b6cf5
Yulianna Shulshenko <yulianna.zhitkovich@mai.ru>
2023-01-12 16:22:02 +0300

    change burger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,8 @@
+let check = document.querySelector(".check");
 let menuBtn = document.querySelector(".menu-btn");
 let menu = document.querySelector(".header-navigation");
 let content = document.querySelector(".content");
-menuBtn.addEventListener("click", function () {
+check.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
   content.classList.toggle("active");
