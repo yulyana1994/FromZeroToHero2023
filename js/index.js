f32b14e7424920dfede5a90fa6891e7c49e8bb5c
Yulianna Shulshenko <yulianna.zhitkovich@mai.ru>
2023-01-13 14:06:29 +0300

    slider

--- js/index.js
+++ js/index.js
@@ -5,11 +5,32 @@ let content = document.querySelector(".content");
 check.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
-  content.classList.toggle("active");
+  content.classList.toggle("unactive");
 });
 
 let changeTheme = document.querySelector(".change-theme");
 let body = document.querySelector("body");
 changeTheme.addEventListener("click", function () {
   body.classList.toggle("dark-theme");
+});
+
+let offset = 0;
+let sliderLine = document.querySelector(".slider-line");
+let sliderNext = document.querySelector(".slider-next");
+let sliderPrev = document.querySelector(".slider-prev");
+
+sliderNext.addEventListener("click", function () {
+  offset = offset + 256;
+  if (offset > 768) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + "px";
+});
+
+sliderPrev.addEventListener("click", function () {
+  offset = offset - 256;
+  if (offset < 0) {
+    offset = 768;
+  }
+  sliderLine.style.left = -offset + "px";
 });
